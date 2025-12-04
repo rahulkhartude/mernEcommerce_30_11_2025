e40ff5c8e87a243ca3d38f15d1e75e2f77284b01
Rahul Khartude <rahulkhartude31@gmail.com>
2025-12-04 08:22:52 +0530

    categories and home design added

---
+++ frontend/src/pages/home/Categories.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import category1 from '../../assets/category-1.jpg'
+import category2 from '../../assets/category-2.jpg'
+import category3 from '../../assets/category-3.jpg'
+import category4 from '../../assets/category-4.jpg'
+import { Link } from 'react-router-dom';
+
+const Categories = () => {
+    const categories = [
+        {name:"Accessories",path:"Accessories", image:category1},
+        {name:"Dress Collection",path:"dress", image:category2},
+        {name:"Jewellery",path:"jewellery", image:category3},
+        {name:"Costmetics",path:"costmetics", image:category4},
+    ]
+  return (
+    <>  
+    <div className='product__grid'>
+         {
+            categories.map((category,index)=>(
+                <Link key={index} to={`/categories/${category.path}`} className='categories__card'>
+                    <img src={category.image} alt={category.name}/>
+                    <h4>{category.name}</h4>
+                </Link>
+            ))
+         }
+    </div>
+    </>
+  )
+}
+
+export default Categories
