d909996aa1a044232ede8e8651fb5365f561654c
rahulkhartude <rahulkhartude31@gmail.com>
2025-12-04 17:42:37 +0530

    trending cards, Products added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Banner from './Banner'
 import Categories from './Categories'
+import HeroSection from './HeroSection'
+import TrendingProducts from '../shop/TrendingProducts'
 
 const Home = () => {
   return (
     <>
       <Banner/>
       <Categories/>
-    </>
+      <HeroSection />
+      <TrendingProducts/>
+          </>
   )
 }
 
---
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -0,0 +1,61 @@
+// import React from 'react'
+// import { Link } from 'react-router';
+
+// const ProductCard = ({products}) => {
+//   console.log("products",products);
+//   return (
+//     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+//       {
+//         products.map((product,index)=>(
+//           <div key={index} className='product_card'>
+//             <div className='relative'>
+//             <Link to={`/shop/${product._id}`}>
+//              <img src={product.image} alt='product image' className='max-h-96 md:h-64 w-full object-cover hover:scale-105 transition-all duration-300' />
+//             </Link>
+          
+//             <div className='hover:block absolute top-3 right-3'>
+            
+//             <button>
+//               <i className="ri-shopping-cart-2-fill bg-primary p-1.5 text-white hover:bg-primary-dark"  ></i>
+//             </button>
+            
+//           </div>
+//           </div>
+//           </div>
+//         ))
+//       }
+//     </div>
+//   )
+// }
+
+// export default ProductCard
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ProductCards = ({products}) => {
+  console.log(products)
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+      {
+        products.map((product, index) => (
+          <div key={index} className='product__card'>
+            <div className='relative'>
+              <Link to={`/shop/${product._id}`}>
+                <img src={product.image} alt="product image" className='max-h-96 md:h-64 w-full object-cover hover:scale-105 transition-all duration-300' />
+              </Link>
+
+              <div className='hover:block absolute top-3 right-3'>
+                <button>
+                  <i className="ri-shopping-cart-2-line bg-primary p-1.5 text-white hover:bg-primary-dark"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default ProductCards
